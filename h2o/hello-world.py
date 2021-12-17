5d458bfb6339adfb2cc7c4d53afd29c0ac1d36f5
Kenichiro Nishioka <nishioka.kenichiro@gmail.com>
2021-12-17 20:52:39 +0900

    Add hello-world.py

---
+++ h2o/hello-world.py
@@ -0,0 +1,31 @@
+from h2o_wave import site, ui
+
+page = site["/hello"]
+
+page["quote"] = ui.markdown_card(
+    box="1 1 1 1",
+    title="Hello World",
+    content="ここに **本文**",
+)
+
+
+# for j in range(1, 5):
+#     for i in range(1, 5):
+#         print(j, i)
+#         page[f"quote{i}_{j}"] = ui.markdown_card(
+#             box=f"{j} {i} 1 1",
+#             title="Hello World",
+#             content=f"{j} {i} 1 1",
+#         )
+
+# for j in range(3):
+#     for i in range(3):
+#         print(j, i)
+#         page[f"quote{i}_{j}"] = ui.markdown_card(
+#             box=f"{j * 2 + 1} {i * 2 + 1} 2 2",
+#             title="Hello World",
+#             content=f"{j * 2 + 1} {i * 2 + 1} 2 2",
+#         )
+
+
+page.save()
