ed2c2a79ce34baa06bd30847476216a2713199d1
Kenichiro Nishioka <nishioka.kenichiro@gmail.com>
2022-05-22 17:40:27 +0900

    Add tweepy auth

--- athena/tweets.py
+++ athena/tweets.py
@@ -5,10 +5,13 @@ import tweepy
 API_KEY = os.environ["TWITTER_API_KEY"]
 API_SECRET = os.environ["TWITTER_API_SECRET"]
 ACCESS_TOKEN = os.environ["TWITTER_ACCESS_TOKEN"]
-ACCESS_TOKEN_SECRET = os.environ["TWITTER_ACCESSTOKEN_SECRET"]
+ACCESS_TOKEN_SECRET = os.environ["TWITTER_ACCESS_TOKEN_SECRET"]
 
 
 def get_tweets(keyword, n_tweets):
+    auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    api = tweepy.API(auth)
     tweets = [
         tweet.text
         for tweet in tweepy.Cursor(api.search_tweets, q=keyword).items(n_tweets)
