5316987cc079a6ab7cfa8879f3c470af78294fed
Kenichiro Nishioka <nishioka.kenichiro@gmail.com>
2022-05-25 07:28:04 +0900

    Write tweet json to file

--- athena/tweets.py
+++ athena/tweets.py
@@ -1,3 +1,4 @@
+import io
 import os
 
 import tweepy
@@ -13,10 +14,13 @@ def get_tweets(keyword, n_tweets):
     auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
     api = tweepy.API(auth)
     tweets = [
-        tweet.text
+        tweet._json
         for tweet in tweepy.Cursor(api.search_tweets, q=keyword).items(n_tweets)
     ]
+    return tweets
 
 
 if __name__ == "__main__":
-    get_tweets(keyword="S&P500", n_tweets=10000)
+    tweets = get_tweets(keyword="S&P500 lang:en", n_tweets=1000)
+    with io.open("tweets.dat", "w") as f:
+        f.write("\n".join(map(str, tweets)))
