d4bca75da2d5d22aa74fa1016e88ff6cb95b7a78
Kenichiro Nishioka <nishioka.kenichiro@gmail.com>
2022-05-26 11:12:48 +0900

    Use lower case for type

--- athena/create_tweets_table.sql
+++ athena/create_tweets_table.sql
@@ -1,7 +1,7 @@
 CREATE EXTERNAL TABLE IF NOT EXISTS tweets (
-    id BIGINT,
-    text STRING,
-    created_at STRING
+    id bigint,
+    text string,
+    created_at string
 )
 ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
 LOCATION 's3://machine-learning-workshop/data/tweets/'
