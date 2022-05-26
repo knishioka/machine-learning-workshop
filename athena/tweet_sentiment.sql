98f7f1c2ecf954fd514081af5ec863359a02a4e5
Kenichiro Nishioka <nishioka.kenichiro@gmail.com>
2022-05-26 14:54:57 +0900

    Count sentiments

--- athena/tweet_sentiment.sql
+++ athena/tweet_sentiment.sql
@@ -1,2 +1,13 @@
-USING EXTERNAL FUNCTION detect_sentiment(text_col VARCHAR, lang VARCHAR) RETURNS VARCHAR LAMBDA 'textanalytics-udf'
-SELECT detect_sentiment(CAST(text AS VARCHAR), 'en') AS sentiment FROM tweets
+USING EXTERNAL FUNCTION detect_sentiment(text_col VARCHAR, lang VARCHAR) RETURNS VARCHAR LAMBDA 'textanalytics-udf' 
+
+WITH tweet_sentiments AS (
+    SELECT
+        detect_sentiment(CAST(text AS VARCHAR), 'en') AS sentiment
+    FROM tweets
+)
+
+SELECT
+    sentiment,
+    COUNT(1)
+FROM tweet_sentiments
+GROUP BY sentiment
